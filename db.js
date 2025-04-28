a1cc8d302d8816dd3a96cb25db22751534c9a01c
Varn1ka <157673718+Varn1ka@users.noreply.github.com>
2025-04-28 11:56:12 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,12 +5,11 @@ const connectDB = async () => {
         await mongoose.connect("mongodb://127.0.0.1:27017/BackEnd", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        }).then(console.log("Connected database")).catch((err=>{
-            console.log("Error In Connection");
-        }))
+        });
+        console.log("Connected to the database successfully");
     } catch (error) {
-        console.error(" Database Connection Error:", error.message);
-        process.exit(1);
+        console.error("Database connection error:", error.message);
+        process.exit(1); // Exit the process if the connection fails
     }
 };
 
